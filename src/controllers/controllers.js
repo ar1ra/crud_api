ea45b4f7526deac5350ffdba3c04652f064dcfaf
r_ubuntu <orroman979@gmail.com>
2023-01-05 14:55:19 +0100

    feat: update server error handle

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -4,145 +4,187 @@ import { STATUS_CODES } from "../constants/index.js";
 import { mathId } from "../utils/index.js";
 
 const getAllUsers = (req, res) => {
-  return res.writeHead(STATUS_CODES.SUCCESS, {
-    "Content-Type": "application/json"
-  }).end(JSON.stringify(db));
+  try {
+    return res.writeHead(STATUS_CODES.SUCCESS, {
+      "Content-Type": "application/json"
+    }).end(JSON.stringify(db));
+  } catch (error) {
+    return res.writeHead(STATUS_CODES.SERVER_ERROR, {
+      "Content-Type": "application/json"
+    }).end(JSON.stringify({ message: "Something went wrong" }));
+  }
 };
 
 const createUser = (req, res) => {
-  let body = "";
-  req.on("data", (data) => {
-    body += data.toString();
-  });
-  req.on("end", () => {
-    if (body) {
-      const data = JSON.parse(body);
-      if (
-        data.name &&
-        data.age &&
-        data.hobbies
-      ) {
-        const createdUser = {
-          id: uuid(),
-          ...JSON.parse(body)
-        };
-
-        db.push(createdUser);
-        return res.writeHead(STATUS_CODES.CREATED, {
-          "Content-Type": "application/json"
-        }).end(JSON.stringify(createdUser));
-      }
-      else {
-        return res.writeHead(STATUS_CODES.BAD_REQUEST, {
-          "Content-Type": "application/json"
-        }).end("Name, age, hobbies are required");
-      }
-    }
-    else {
-      return res.writeHead(STATUS_CODES.BAD_REQUEST, {
-        "Content-Type": "application/json"
-      }).end("Name, age, hobbies are required");
-    }
-  });
-
-  req.on("error", () => {
-    return res.writeHead(STATUS_CODES.SERVER_ERROR, {
-      "Content-Type": "application/json"
-    }).end("Something went wrong");
-  });
-};
-
-const getUser = (req, res, id) => {
-  if (mathId(id)) {
-    const user = db.find(el => el.id === id);
-    if (!user) {
-      return res.writeHead(STATUS_CODES.NOT_FOUND, {
-        "Content-Type": "application/json"
-      }).end(`User with ID: ${id} not found`);
-    }
-
-    return res.writeHead(STATUS_CODES.SUCCESS, {
-      "Content-Type": "application/json"
-    }).end(JSON.stringify(user));
-  }
-
-  return res.writeHead(STATUS_CODES.BAD_REQUEST, {
-    "Content-Type": "application/json"
-  }).end("Wrong user ID");
-};
-
-const updateUser = (req, res, id) => {
-  if (mathId(id)) {
+  try {
     let body = "";
     req.on("data", (data) => {
       body += data.toString();
     });
     req.on("end", () => {
+      if (body) {
+        try {
+          const data = JSON.parse(body);
+          if (
+            data.name &&
+            data.age &&
+            data.hobbies
+          ) {
+            const createdUser = {
+              id: uuid(),
+              ...JSON.parse(body)
+            };
+
+            db.push(createdUser);
+            return res.writeHead(STATUS_CODES.CREATED, {
+              "Content-Type": "application/json"
+            }).end(JSON.stringify(createdUser));
+          }
+          else {
+            return res.writeHead(STATUS_CODES.BAD_REQUEST, {
+              "Content-Type": "application/json"
+            }).end(JSON.stringify({ message: "Name, age, hobbies are required" }));
+          }
+        } catch (error) {
+          return res.writeHead(STATUS_CODES.SERVER_ERROR, {
+            "Content-Type": "application/json"
+          }).end(JSON.stringify({ message: "Something went wrong" }));
+        }
+      }
+      else {
+        return res.writeHead(STATUS_CODES.BAD_REQUEST, {
+          "Content-Type": "application/json"
+        }).end(JSON.stringify({ message: "Name, age, hobbies are required" }));
+      }
+    });
+
+    req.on("error", () => {
+      return res.writeHead(STATUS_CODES.SERVER_ERROR, {
+        "Content-Type": "application/json"
+      }).end(JSON.stringify({ message: "Something went wrong" }));
+    });
+  } catch (error) {
+    return res.writeHead(STATUS_CODES.SERVER_ERROR, {
+      "Content-Type": "application/json"
+    }).end(JSON.stringify({ message: "Something went wrong" }));
+  }
+};
+
+const getUser = (req, res, id) => {
+  try {
+    if (mathId(id)) {
       const user = db.find(el => el.id === id);
       if (!user) {
         return res.writeHead(STATUS_CODES.NOT_FOUND, {
           "Content-Type": "application/json"
-        }).end(`User with ID: ${id} not found`);
+        }).end(JSON.stringify({ message: `User with ID: ${id} not found` }));
       }
-      else {
-        if (body) {
-          const data = JSON.parse(body);
-          const userIndex = db.findIndex(el => el.id === id);
 
-          const updatedUser = {
-            id,
-            name: data.name || user.name,
-            age: data.age || user.age,
-            hobbies: data.hobbies || user.hobbies
-          };
-
-          db[userIndex] = updatedUser;
-
-          return res.writeHead(STATUS_CODES.SUCCESS, {
-            "Content-Type": "application/json"
-          }).end(JSON.stringify(updatedUser));
-        }
-        else {
-          return res.writeHead(STATUS_CODES.SUCCESS, {
-            "Content-Type": "application/json"
-          }).end("No body");
-        }
-      }
-    });
-
-    req.on("error", (error) => {
-      return res.writeHead(STATUS_CODES.SERVER_ERROR, {
+      return res.writeHead(STATUS_CODES.SUCCESS, {
         "Content-Type": "application/json"
-      }).end(JSON.stringify(error));
-    });
-  }
-  else {
+      }).end(JSON.stringify(user));
+    }
+
     return res.writeHead(STATUS_CODES.BAD_REQUEST, {
       "Content-Type": "application/json"
-    }).end("Wrong user ID");
+    }).end(JSON.stringify({ message: "User ID is invalid" }));
+  } catch (error) {
+    return res.writeHead(STATUS_CODES.SERVER_ERROR, {
+      "Content-Type": "application/json"
+    }).end(JSON.stringify({ message: "Something went wrong" }));
+  }
+};
+
+const updateUser = (req, res, id) => {
+  try {
+    if (mathId(id)) {
+      let body = "";
+      req.on("data", (data) => {
+        body += data.toString();
+      });
+      req.on("end", () => {
+        const user = db.find(el => el.id === id);
+        if (!user) {
+          return res.writeHead(STATUS_CODES.NOT_FOUND, {
+            "Content-Type": "application/json"
+          }).end(JSON.stringify({ message: `User with ID: ${id} not found` }));
+        }
+        else {
+          if (body) {
+            try {
+              const data = JSON.parse(body);
+              const userIndex = db.findIndex(el => el.id === id);
+
+              const updatedUser = {
+                id,
+                name: data.name || user.name,
+                age: data.age || user.age,
+                hobbies: data.hobbies || user.hobbies
+              };
+
+              db[userIndex] = updatedUser;
+
+              return res.writeHead(STATUS_CODES.SUCCESS, {
+                "Content-Type": "application/json"
+              }).end(JSON.stringify(updatedUser));
+            } catch (error) {
+              return res.writeHead(STATUS_CODES.SERVER_ERROR, {
+                "Content-Type": "application/json"
+              }).end(JSON.stringify({ message: "Something went wrong" }));
+            }
+          }
+          else {
+            return res.writeHead(STATUS_CODES.NO_CONTENT, {
+              "Content-Type": "application/json"
+            }).end();
+          }
+        }
+      });
+
+      req.on("error", (error) => {
+        return res.writeHead(STATUS_CODES.SERVER_ERROR, {
+          "Content-Type": "application/json"
+        }).end(JSON.stringify({ message: "Something went wrong" }));
+      });
+    }
+    else {
+      return res.writeHead(STATUS_CODES.BAD_REQUEST, {
+        "Content-Type": "application/json"
+      }).end(JSON.stringify({ message: "User ID is invalid" }));
+    }
+  } catch (error) {
+    return res.writeHead(STATUS_CODES.SERVER_ERROR, {
+      "Content-Type": "application/json"
+    }).end(JSON.stringify({ message: "Something went wrong" }));
   }
 };
 
 const deleteUser = (req, res, id) => {
-  if (mathId(id)) {
-    const user = db.find(el => el.id === id);
-    if (!user) {
-      return res.writeHead(STATUS_CODES.NOT_FOUND, {
+  try {
+    if (mathId(id)) {
+      const user = db.find(el => el.id === id);
+      if (!user) {
+        return res.writeHead(STATUS_CODES.NOT_FOUND, {
+          "Content-Type": "application/json"
+        }).end(JSON.stringify({ message: `User with ID: ${id} not found` }));
+      }
+
+      const deletedUserIndex = db.findIndex(el => el.id === user.id);
+      db.splice(deletedUserIndex, 1);
+
+      return res.writeHead(STATUS_CODES.NO_CONTENT, {
         "Content-Type": "application/json"
-      }).end(`User with ID: ${id} not found`);
+      }).end();
     }
-    
-    const deletedUserIndex = db.findIndex(el => el.id === user.id);
-    db.splice(deletedUserIndex, 1);
 
-    return res.writeHead(STATUS_CODES.DELETED, {
+    return res.writeHead(STATUS_CODES.BAD_REQUEST, {
       "Content-Type": "application/json"
-    }).end();
+    }).end(JSON.stringify({ message: "User ID is invalid" }));
+  } catch (error) {
+    return res.writeHead(STATUS_CODES.SERVER_ERROR, {
+      "Content-Type": "application/json"
+    }).end(JSON.stringify({ message: "Something went wrong" }));
   }
-
-  return res.writeHead(STATUS_CODES.BAD_REQUEST, {
-    "Content-Type": "application/json"
-  }).end("Wrong user ID");
 };
 
 export {
